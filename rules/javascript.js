35c5623f49ea04091e941544e04b7aa9339dcccd
charlieroberts <charlie@charlie-roberts.com>
2024-11-16 13:48:15 -0800

    reformat to dictionary

--- rules/javascript.js
+++ rules/javascript.js
@@ -1,14 +1,9 @@
-window.bitty.rules =  [
-  // comments
-  [ /(\/\/.*)/g, '<span class=bitty-comments>$1</span>'],
+window.bitty.rules = {
+  comments: /(\/\/.*)/g,
+  
+  keywords: /\b(new|if|else|do|while|switch|for|of|continue|break|return|typeof|function|var|const|let|\.length)(?=[^\w])/g,
 
-  // keywords and properties
-  [ /\b(new|if|else|do|while|switch|for|of|continue|break|return|typeof|function|var|const|let|\.length)(?=[^\w])/g, "<span class=bitty-keywords>$1</span>"],
+  numbers: /\b(\d+)/g,
 
-  // strings
-  // extraneous multiline comment below to remove syntax highlight bug in vim
-  [ /(".*?"|'.*?'|\`.*?\`)/g, '<span class=bitty-strings>$1</span>' /*`*/],
-
-  // numbers
-  [ /\b(\d+)/g, '<span class=bitty-numbers>$1</span>']
-]
+  strings: /(".*?"|'.*?'|\`.*?\`)/g
+}
