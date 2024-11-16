ba87e5cee5fed1181154b60d3e5856cdc73d093d
charlieroberts <charlie@charlie-roberts.com>
2024-11-15 16:33:36 -0800

    removing comment rule until debugged

--- rules/javascript.js
+++ rules/javascript.js
@@ -1,6 +1,6 @@
 window.bitty.rules =  [
   // comments
-  [ /(\/\/.*)/g, '<em>$1</em>'],
+  //[ /(\/\/.*)/g, '<em>$1</em>'],
 
   // keywords and properties
   [ /\b(new|if|else|do|while|switch|for|in|of|continue|break|return|typeof|function|var|const|let|\.length|\.\w+)(?=[^\w])/g, '<strong>$1</strong>'],
